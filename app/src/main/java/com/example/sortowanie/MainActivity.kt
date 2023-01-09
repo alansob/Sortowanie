ddddb112df5e3fbedff2c983f5b54a1c6167972f
Alan Sobczak <Asobczak020304@gmial.com>
2023-01-09 17:56:25 +0100

    GUI

--- app/src/main/java/com/example/sortowanie/MainActivity.kt
+++ app/src/main/java/com/example/sortowanie/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.sortowanie
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
